6b662007f40c7b2fd32d4ed69b525ee9094d8812
iecube-zhuxiaoxi <xiaoxi.zhu@iecube.com.cn>
2025-07-09 17:22:13 +0800

    :rocket: 讲义代码下方增加编辑框

--- src/views/digitalPlan/resizePanel.vue
+++ src/views/digitalPlan/resizePanel.vue
@@ -1,117 +1,133 @@
 <template>
   <div class="resizable-container" ref="container">
-    <div class="panel left-panel" ref="leftPanel">
+    <div v-show="!rightPanelOnly" ref="leftPanel" class="panel left-panel" >
       <div class="mask" v-show="isDragging"></div>
       <slot name="left-content" />
     </div>
-    
-    <div 
-      class="resize-handle" 
+
+    <div
+      v-show="!rightPanelOnly"
+      class="resize-handle"
       ref="resizeHandle"
       @mousedown="startDrag"
       @touchstart="startDrag"
     />
-    
+
     <div class="panel right-panel" ref="rightPanel">
       <div class="mask" v-show="isDragging"></div>
-      <slot name="right-content"  />
+      <slot name="right-content" />
     </div>
   </div>
 </template>
 
 <script setup>
-import { ref, onMounted, onBeforeUnmount } from 'vue';
+  import { ref, onMounted, onBeforeUnmount } from 'vue'
 
-const container = ref(null);
-const leftPanel = ref(null);
-const resizeHandle = ref(null);
-const rightPanel = ref(null);
-
-let isDragging = ref(false);
-let startX = 0;
-let startWidth = 0;
-
-const startDrag = (e) => {
-  isDragging.value = true;
-  startX = e.type === 'mousedown' ? e.clientX : e.touches[0].clientX;
-  startWidth = leftPanel.value.offsetWidth;
+  const rightPanelOnly = ref(false)
   
-  document.addEventListener('mousemove', doDrag);
-  document.addEventListener('touchmove', doDrag);
-  document.addEventListener('mouseup', stopDrag);
-  document.addEventListener('touchend', stopDrag);
-  
-  e.preventDefault();
-};
-
-const doDrag = (e) => {
-  if (!isDragging.value) return;
-  
-  const currentX = e.type === 'mousemove' ? e.clientX : e.touches[0].clientX;
-  const newWidth = startWidth + (currentX - startX);
-  
-  const minWidth = 150;
-  const maxWidth = container.value.offsetWidth - minWidth;
-  
-  if (newWidth >= minWidth && newWidth <= maxWidth) {
-    leftPanel.value.style.width = `${newWidth}px`;
+  const toggleRightPanelOnly = () => {
+    rightPanelOnly.value = !rightPanelOnly.value
   }
-};
+  
+  const setRightPanelOnly = value => {
+    rightPanelOnly.value = value
+  }
+  
+  defineExpose({
+    toggleRightPanelOnly,
+    setRightPanelOnly
+  })
 
-const stopDrag = () => {
-  isDragging.value = false;
-  document.removeEventListener('mousemove', doDrag);
-  document.removeEventListener('touchmove', doDrag);
-  document.removeEventListener('mouseup', stopDrag);
-  document.removeEventListener('touchend', stopDrag);
-};
+  const container = ref(null)
+  const leftPanel = ref(null)
+  const resizeHandle = ref(null)
+  const rightPanel = ref(null)
 
-onMounted(() => {
-  leftPanel.value.style.width = '50%';
-});
+  let isDragging = ref(false)
+  let startX = 0
+  let startWidth = 0
 
-onBeforeUnmount(() => {
-  document.removeEventListener('mousemove', doDrag);
-  document.removeEventListener('touchmove', doDrag);
-  document.removeEventListener('mouseup', stopDrag);
-  document.removeEventListener('touchend', stopDrag);
-});
+  const startDrag = e => {
+    isDragging.value = true
+    startX = e.type === 'mousedown' ? e.clientX : e.touches[0].clientX
+    startWidth = leftPanel.value.offsetWidth
+
+    document.addEventListener('mousemove', doDrag)
+    document.addEventListener('touchmove', doDrag)
+    document.addEventListener('mouseup', stopDrag)
+    document.addEventListener('touchend', stopDrag)
+
+    e.preventDefault()
+  }
+
+  const doDrag = e => {
+    if (!isDragging.value) return
+
+    const currentX = e.type === 'mousemove' ? e.clientX : e.touches[0].clientX
+    const newWidth = startWidth + (currentX - startX)
+
+    const minWidth = 150
+    const maxWidth = container.value.offsetWidth - minWidth
+
+    if (newWidth >= minWidth && newWidth <= maxWidth) {
+      leftPanel.value.style.width = `${newWidth}px`
+    }
+  }
+
+  const stopDrag = () => {
+    isDragging.value = false
+    document.removeEventListener('mousemove', doDrag)
+    document.removeEventListener('touchmove', doDrag)
+    document.removeEventListener('mouseup', stopDrag)
+    document.removeEventListener('touchend', stopDrag)
+  }
+
+  onMounted(() => {
+    leftPanel.value.style.width = '50%'
+  })
+
+  onBeforeUnmount(() => {
+    document.removeEventListener('mousemove', doDrag)
+    document.removeEventListener('touchmove', doDrag)
+    document.removeEventListener('mouseup', stopDrag)
+    document.removeEventListener('touchend', stopDrag)
+  })
 </script>
 
 <style scoped lang="scss">
-.resizable-container {
-  height: 100%;
-  display: flex;
-}
+  .resizable-container {
+    height: 100%;
+    display: flex;
+  }
 
-.panel {
-  position: relative;
-  overflow: hidden;
-}
+  .panel {
+    position: relative;
+    overflow: hidden;
+  }
 
-.right-panel {
-  width: 0;
-  flex: 1;
-  padding: 0 8px;
-}
+  .right-panel {
+    width: 0;
+    flex: 1;
+    padding: 0 8px;
+  }
 
-.resize-handle {
-  width: 5px;
-  background-color: #ccc;
-  cursor: col-resize;
-}
+  .resize-handle {
+    width: 5px;
+    background-color: #ccc;
+    cursor: col-resize;
+  }
 
-.resize-handle:hover {
-  width: 5px;
-  background-color: var(--zeng);
-}
+  .resize-handle:hover {
+    width: 5px;
+    background-color: var(--zeng);
+  }
 
-.mask {
-  position: absolute;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
-  z-index: 999;
-}
+  .mask {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    z-index: 999;
+  }
 </style>
--- src/views/digitalPlan/showDigitalPlan.vue
+++ src/views/digitalPlan/showDigitalPlan.vue
@@ -1,96 +1,136 @@
 <template>
   <div class="plan-container">
     <div class="btn-tools">
-      <div class="w-0 flex-1">
-        <span v-show="showloading" class="loading-text">{{ loadingText }}</span>
-      </div>
+      <div></div>
 
       <div class="h-full flex items-center gap-2">
-        <el-icon
-          size="20px"
-          class="hovrer:cursor-pointer hover:text-blue-500"
-          title="保存"
-          v-show="editorVisible"
-          @click="handleSave"
+        <Icon
+          title="代码"
+          :size="iconSize"
+          class="btn-icon"
+          :class="{ isActive: !isPreview }"
+          @click="handlePreview(false)"
         >
-          <FolderChecked />
-        </el-icon>
-        <el-icon
-          size="20px"
-          class="hovrer:cursor-pointer hover:text-blue-500"
-          title="编辑模式"
-          v-show="!editorVisible"
-          @click="openEditor"
+          <ClipboardCode20Filled />
+        </Icon>
+        <Icon
+          title="预览"
+          :size="iconSize"
+          class="btn-icon"
+          :class="{ isActive: isPreview }"
+          @click="handlePreview(true)"
         >
-          <Edit />
-        </el-icon>
-        <!-- <el-icon size="20px" class="hovrer:cursor-pointer hover:text-blue-500" title="重新生成" @click="handleRefresh">
-          <Refresh />
-        </el-icon> -->
-        <el-icon
-          size="20px"
-          class="hovrer:cursor-pointer hover:text-blue-500"
-          title="下载页面"
-          @click="handleDownload"
-        >
+          <Eye />
+        </Icon>
+        <Icon title="保存" :size="iconSize" class="btn-icon" @click="handleSave">
+          <Save />
+        </Icon>
+        <Icon title="下载" :size="iconSize" class="btn-icon" @click="handleDownload">
           <Download />
-        </el-icon>
-        <!--  <el-icon size="20px" class="hovrer:cursor-pointer hover:text-blue-500" title="分享" v-loading="shortLoading"
-          @click="handleShare">
-          <Share />
-        </el-icon> -->
-        <!-- <el-icon size="20px" class="hovrer:cursor-pointer hover:text-blue-500" title="分享">
-          <Share />
-        </el-icon> -->
+        </Icon>
       </div>
     </div>
 
-    <resizePanel class="content-wrapper">
+    <resizePanel ref="resizePanelRef" class="content-wrapper">
       <template #left-content>
-        <div ref="editorRef" class="h-full"></div>
+        <div class="wh-full relative flex flex-col bg-neutral-800">
+          <div ref="editorRef" class="h-0 flex-1"></div>
+          <div class="px-3 my-2">
+            <div
+              class="relative bg-neutral-800 border border-neutral-700 rounded-2xl ring-[4px] focus-within:ring-neutral-500/30 focus-within:border-neutral-600 ring-transparent z-10 w-full group"
+            >
+              <div class="px-4 pt-3">
+                <div
+                  class="border border-neutral-700 rounded-xl p-1.5 pr-3 max-w-max hover:brightness-110 transition-all duration-200 ease-in-out !cursor-pointer !cursor-pointer"
+                ></div>
+              </div>
+              <div class="w-full relative flex items-center justify-between">
+                <el-input
+                  type="text"
+                  class="ask-text-input"
+                  v-model="askText"
+                  placeholder="请输入要替换的内容"
+                />
+              </div>
+              <div class="flex items-center justify-between gap-2 px-4 pb-3">
+                <div class="flex-1 flex items-center justify-start gap-2">
+                  <div class="ask-btn">
+                    <Icon>
+                      <Crosshairs />
+                    </Icon>
+                  </div>
+                </div>
+                <div class="flex items-center justify-end gap-2">
+                  <div class="ask-btn">
+                    <Icon>
+                      <SendAltFilled />
+                    </Icon>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </template>
       <template #right-content>
-        <iframe
-          ref="iframeRef"
-          class="w-full select-none transition-all duration-200 bg-black h-full"
-          :src="relativeFilePath + `?t=${t}`"
-          loading="lazy"
-          @load="closeLoadingText"
-        ></iframe>
+        <div class="wh-full relative">
+          <el-skeleton
+            v-if="loading"
+            class="wh-full absolute top-0 left-0 z-500"
+            style="background-color: var(--bg-color)"
+            :loading="loading"
+            animated
+            :throttle="{ leading: 500, initVal: true }"
+          >
+            <template #template>
+              {{ loading }}
+              <div class="loading-text">{{ loadingText }}</div>
+              <el-skeleton-item variant="h1" style="width: 100%" class="mt-4 mb-4" />
+              <el-skeleton-item variant="text" style="width: 100%" />
+              <el-skeleton-item variant="text" style="width: 100%" />
+              <el-skeleton-item variant="text" style="width: 100%" />
+              <el-skeleton-item variant="text" style="width: 80%" />
+            </template>
+          </el-skeleton>
+          <iframe
+            ref="iframeRef"
+            class="wh-full absolute top-0 left-0"
+            :src="relativeFilePath + `?t=${t}`"
+            loading="lazy"
+            @load="stopLoading"
+          ></iframe>
+        </div>
       </template>
     </resizePanel>
-
-    <el-dialog v-model="shareVisible" title="分享" align-center>
-      <div class="w-full h-full">
-        <img :src="screenshotUrl" class="w-full h-full" />
-        <img :src="huichuang" class="absolute top-[100px] right-0 w-[160px] h-[160px]" />
-      </div>
-    </el-dialog>
   </div>
 </template>
 
 <script setup>
+  import { Download, Save, SendAltFilled } from '@vicons/carbon'
+  import { Crosshairs, Eye } from '@vicons/fa'
+  import { ClipboardCode20Filled } from '@vicons/fluent'
   import resizePanel from './resizePanel.vue'
 
-  // import { EditorView, basicSetup } from 'codemirror'
-  // import { EditorState } from '@codemirror/state'
-  // import { oneDark } from '@codemirror/theme-one-dark';
-  // import { css } from '@codemirror/lang-css';
-  // import { html } from '@codemirror/lang-html';
-  // import { javascript } from '@codemirror/lang-javascript'
   import { Base64 } from 'js-base64'
   import { updatePlan } from '@/api/plan'
-  import html2canvas from 'html2canvas'
-  import huichuang from './huichuang.jpg'
 
   import * as monaco from 'monaco-editor/esm/vs/editor/editor.main.js'
 
+  // 左侧编辑器
+  const askText = ref('')
+
+  const iconSize = ref(20)
+  const isPreview = ref(false)
+  const resizePanelRef = ref(null)
+
+  const handlePreview = v => {
+    isPreview.value = v
+    resizePanelRef.value.setRightPanelOnly(v)
+  }
+
   const iframeRef = ref(null)
 
-  const screenshotUrl = ref('')
-  const shortLoading = ref(false)
-
-  const showloading = ref(true)
+  const loading = ref(true)
   const loadingText = ref('正在为您拼命加载页面中.....')
   const t = ref('')
 
@@ -100,12 +140,12 @@
 
   updateTime()
 
-  const showLoadingText = () => {
-    showloading.value = true
+  const startLoading = () => {
+    loading.value = true
   }
 
-  const closeLoadingText = async () => {
-    // showloading.value = false
+  const stopLoading = async () => {
+    loading.value = false
     // await nextTick()
     // setTimeout(async () => {
     //   await short()
@@ -114,6 +154,7 @@
 
   const route = useRoute()
   const relativeFilePath = ref('')
+  const textValue = ref('')
   const fileName = ref('')
   const id = ref(null)
   relativeFilePath.value = route.query.filePath
@@ -123,29 +164,6 @@
   const editorVisible = ref(true)
   const editorRef = ref(null)
   let editorInstance = null
-
-  // let editorState = null
-  // let editorView = null
-  const textValue = ref('')
-
-  /* const createEditor = (text) => {
-  editorState = EditorState.create({
-    doc: text,
-    extensions: [
-      basicSetup,
-      css(),
-      html(),
-      javascript(),
-      oneDark,
-    ],
-  });
-
-  editorView = new EditorView({
-    state: editorState,
-    parent: editorRef.value
-  })
-
-} */
 
   // 打开关闭编辑器
   const openEditor = () => {
@@ -166,7 +184,8 @@
 
   // 保存文件
   const handleSave = () => {
-    closeEditor()
+    /*  
+   closeEditor()
 
     const content = editorView.state.doc.toString()
     const base64Content = Base64.encode(content)
@@ -175,120 +194,15 @@
       htmlContentBase64: base64Content
     }).then(res => {
       ElMessage.success('保存成功')
-      showLoadingText()
-      updateTime()
-    })
+      handleRefresh()
+    }) 
+      */
   }
 
   // 刷新页面
   const handleRefresh = () => {
-    showLoadingText()
+    startLoading()
     updateTime()
-  }
-
-  // 分享功能
-  const shareVisible = ref(false)
-  const handleShare = async () => {
-    shortLoading.value = true
-
-    if (screenshotUrl.value === '') {
-      console.log('截图失败>>>>>>>>>>>')
-      await short()
-    }
-    shareVisible.value = true
-    shortLoading.value = false
-  }
-
-  const short = async () => {
-    console.log('截图开始>>>>>>>>>>>')
-    let iframe = iframeRef.value
-    const doc = iframe.contentDocument
-
-    // 阶段1：等待基础DOM就绪
-    await waitForDOMReady(iframe)
-
-    // 阶段2：等待CSS/JS资源
-    // await waitForAssets(iframe)
-
-    // 阶段3：等待动态内容
-    await waitForDynamicContent(iframe)
-    // 强制展开滚动区域
-    doc.body.style.overflow = 'visible'
-    doc.body.style.height = 'auto'
-
-    try {
-      const canvas = await html2canvas(iframe.contentWindow.document.body, {
-        useCORS: true,
-        scrollX: window.scrollX,
-        scrollY: window.scrollY,
-        windowWidth: iframe.contentDocument.documentElement.scrollWidth,
-        windowHeight: iframe.contentDocument.documentElement.scrollHeight
-      })
-      screenshotUrl.value = canvas.toDataURL('image/png')
-    } catch (e) {
-      console.log('截图失败>>>>>>>>>>>')
-      screenshotUrl.value = ''
-    }
-  }
-
-  // 等待DOM就绪
-  const waitForDOMReady = iframe => {
-    return new Promise(resolve => {
-      if (iframe.contentDocument.readyState === 'complete') {
-        resolve()
-      } else {
-        iframe.contentDocument.addEventListener('DOMContentLoaded', resolve)
-      }
-    })
-  }
-
-  // 等待CSS/JS资源
-  const waitForAssets = async iframe => {
-    const doc = iframe.contentDocument
-    const scripts = Array.from(doc.querySelectorAll('script'))
-    const links = Array.from(doc.querySelectorAll('link'))
-
-    const scriptPromises = scripts.map(script => {
-      return new Promise((resolve, reject) => {
-        script.addEventListener('load', resolve)
-        script.addEventListener('error', reject)
-      })
-    })
-
-    const linkPromises = links.map(link => {
-      return new Promise((resolve, reject) => {
-        link.addEventListener('load', resolve)
-        link.addEventListener('error', reject)
-      })
-    })
-
-    await Promise.all(...scriptPromises, ...linkPromises)
-  }
-
-  // 等待动态内容
-  const waitForDynamicContent = iframe => {
-    return new Promise(resolve => {
-      // const observer = new MutationObserver(() => {
-      //   resolve()
-      // })
-      // observer.observe(iframe.contentDocument.body, {
-      //   childList: true,
-      //   subtree: true
-      // })
-      // 超时保险
-      setTimeout(() => {
-        resolve()
-      }, 3000)
-    })
-  }
-
-  // 获取讲义文件内容
-  const getHtmlFileContent = async () => {
-    console.log(relativeFilePath.value)
-    const response = await fetch(relativeFilePath.value)
-    const htmlContent = await response.text()
-    textValue.value = htmlContent
-    createEditor(htmlContent)
   }
 
   // 初始化编辑器
@@ -307,62 +221,18 @@
     })
   }
 
-  const contentWrapperRef = ref(null)
-  const resizeBtnRef = ref(null)
-
-  // 存储初始位置和宽度
-  let startX, startWidth
-
-  // 拖拽功能
-  const startDrag = e => {
-    console.log(e)
-    // 添加激活样式FF
-    // resizeHandle.classList.add('active');
-
-    // 记录初始位置和左侧面板宽度
-    startX = e.clientX
-    console.log(`startX: ${startX}`)
-
-    startWidth = parseInt(document.defaultView.getComputedStyle(editorRef.value).width, 10)
-
-    // 添加事件监听
-    document.addEventListener('mousemove', doDrag)
-    document.addEventListener('mouseup', stopDrag)
-
-    // 防止文本选择
-    e.preventDefault()
-  }
-
-  // 拖拽中
-  function doDrag(e) {
-    // 计算新的左侧面板宽度
-    const newWidth = startWidth + (e.clientX - startX)
-    const containerWidth = contentWrapperRef.value.clientWidth
-
-    // 设置最小和最大宽度限制
-    const minWidth = 200
-    const maxWidth = containerWidth - 300
-
-    if (newWidth > minWidth && newWidth < maxWidth) {
-      editorRef.value.style.width = `${newWidth}px`
-    }
-  }
-
-  // 停止拖拽
-  function stopDrag() {
-    // 移除激活样式
-    // resizeHandle.classList.remove('active')
-
-    // 移除事件监听
-    document.removeEventListener('mousemove', doDrag)
-    document.removeEventListener('mouseup', stopDrag)
-
-    console.log('移出监听事件')
+  // 获取讲义文件内容
+  const getHtmlFileContent = async () => {
+    console.log(relativeFilePath.value)
+    const response = await fetch(relativeFilePath.value)
+    const htmlContent = await response.text()
+    textValue.value = htmlContent
+    createEditor(htmlContent)
   }
 
   // 初始化
   const init = () => {
-    showLoadingText()
+    startLoading()
     getHtmlFileContent()
   }
 
@@ -370,7 +240,7 @@
 </script>
 
 <style lang="scss" scoped>
-  $btn-tool-height: 20px;
+  $btn-tool-height: 24px;
 
   .plan-container {
     height: 100%;
@@ -380,14 +250,38 @@
   .btn-tools {
     height: $btn-tool-height;
     @apply flex justify-between;
+    padding-right: 8px;
+  }
 
-    .loading-text {
-      font-size: 16px;
-      @apply whitespace-nowrap font-bold color-blue-500;
-    }
+  .loading-text {
+    font-size: 16px;
+    @apply whitespace-nowrap font-bold color-blue-500;
   }
 
   .content-wrapper {
     height: calc(100% - $btn-tool-height);
+  }
+
+  .btn-icon:hover {
+    cursor: pointer;
+    color: var(--zeng);
+  }
+
+  .isActive {
+    color: var(--zeng);
+  }
+
+  .ask-text-input {
+    :deep(.el-input__wrapper) {
+      @apply bg-transparent border-none rounded-none shadow-none;
+    }
+    :deep(.el-input__inner) {
+      @apply w-full bg-transparent outline-none text-neutral-200 placeholder:text-neutral-500 p-4 !pt-2.5;
+    }
+  }
+
+  .ask-btn {
+    background-color: rgb(240, 240, 240);
+    @apply h24px w24px flex justify-center items-center bg-buttonface  rounded-full cursor-pointer hover:text-zeng;
   }
 </style>
